4c13da1499c5d8fc7b7ddd5aacca25fcb84cef66
ktakada42 <ktakada950711@gmail.com>
2023-03-05 03:02:55 +0900

    feat: reply as ChatGPT

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const express = require("express");
 const line = require("@line/bot-sdk");
 const PORT = process.env.PORT || 3000;
+const { Configuration, OpenAIApi } = require("openai");
 
 const config = {
   channelSecret: process.env.CHANNEL_SECRET,
@@ -28,9 +29,19 @@ async function handleEvent(event) {
     return Promise.resolve(null);
   }
 
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const reply = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: event.message.text }],
+  });
+
   return client.replyMessage(event.replyToken, {
     type: "text",
-    text: event.message.text, //実際に返信の言葉を入れる箇所
+    text: reply.data.choices[0].message.content.trim(), //実際に返信の言葉を入れる箇所
   });
 }
 
