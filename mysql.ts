d12e08d0758335c0dc14107721741f5f624f60d3
ktakada42 <ktakada950711@gmail.com>
2023-03-23 23:52:03 +0900

    fix: treat each query as 1 transaction

--- mysql.ts
+++ mysql.ts
@@ -8,4 +8,4 @@ export const dateTime3Format = "YYYY-MM-DD HH:mm:ss.SSS";
 export const insertQ = "INSERT INTO messages_history VALUES (?, ?, ?, ?, ?)";
 
 export const selectQ =
-  "SELECT role, content FROM messages_history WHERE userId = ? ORDER BY typedAt ASC LIMIT 16";
+  "SELECT role, content, typedAt FROM messages_history WHERE userId = ? ORDER BY typedAt DESC LIMIT 17";
